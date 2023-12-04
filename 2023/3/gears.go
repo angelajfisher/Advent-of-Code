58cdab8987b2ea854cd2cda8ed810b171db669a9
Angela Fisher <31549337+angelajfisher@users.noreply.github.com>
2023-12-04 01:28:12 -0800

    Advent day 3 (part two)

--- 2023/3/gears.go
+++ 2023/3/gears.go
@@ -10,6 +10,7 @@ import (
 
 type schematic struct {
 	symbols []int
+	gearSymbols []int
 	numberIndexes [][]int
 	numbers []string
 }
@@ -25,7 +26,85 @@ func main() {
 	for i := 0; i < len(partNums); i++ {
 		sum += partNums[i]
 	}
-	fmt.Printf("The answer to part one is: %v", sum)
+	fmt.Printf("The answer to part one is: %v\n", sum)
+
+	ratio := 0
+	for _, num := range grabGears(schematics, lastIndex) {
+		ratio += num
+	}
+	fmt.Printf("The answer to part two is: %v", ratio)
+}
+
+// This spaghetti takes all the schematics and returns an array
+// of ratios for the actual gear parts
+func grabGears(schematics []schematic, lastIndex int) []int {
+	gears := []int{}
+
+	for i := 0; i < len(schematics); i++ {
+		for _, index := range schematics[i].gearSymbols {
+			nearby := []int{}
+
+			nums := neighboringNums(index, lastIndex, schematics[i].numberIndexes)
+			if nums[0] >= 0 {
+				num, _ := strconv.Atoi(schematics[i].numbers[nums[0]])
+				nearby = append(nearby, num)
+				if nums[1] >= 0 {
+					num, _ := strconv.Atoi(schematics[i].numbers[nums[1]])
+					nearby = append(nearby, num)
+				}
+			}
+			if i != 0 {
+				nums = neighboringNums(index, lastIndex, schematics[i-1].numberIndexes)
+				if nums[0] >= 0 {
+					num, _ := strconv.Atoi(schematics[i-1].numbers[nums[0]])
+					nearby = append(nearby, num)
+					if nums[1] >= 0 {
+						num, _ := strconv.Atoi(schematics[i-1].numbers[nums[1]])
+						nearby = append(nearby, num)
+					}
+				}
+			}
+			if i != 0 {
+				nums = neighboringNums(index, lastIndex, schematics[i+1].numberIndexes)
+				if nums[0] >= 0 {
+					num, _ := strconv.Atoi(schematics[i+1].numbers[nums[0]])
+					nearby = append(nearby, num)
+					if nums[1] >= 0 {
+						num, _ := strconv.Atoi(schematics[i+1].numbers[nums[1]])
+						nearby = append(nearby, num)
+					}
+				}
+			}
+			if len(nearby) > 1 {
+				gears = append(gears, (nearby[0] * nearby[1]))
+			}
+		}
+	}
+	return gears
+}
+
+// neighboringNums takes a line's start and end values for each num
+// (as an array of arrays) as well as the index of a gear symbol and
+// the final index of the line, search area, then returns up to two
+// indexes of neighboring numbers as an array
+func neighboringNums(index int, lastIndex int, numberIndexes [][]int) [2]int {
+	start := index - 1
+	end := index + 1
+	gearNumIdx := [2]int{-1, -1}
+
+	for i := 0; i < len(numberIndexes); i++ {
+		current := numberIndexes[i]
+
+		if (current[0] >= start && current[0] <= end) || (current[1]-1 >= start && current[1]-1 <= end) {
+			if gearNumIdx[0] > -1 {
+				gearNumIdx[1] = i
+				return gearNumIdx
+			} else {
+				gearNumIdx[0] = i
+			}
+		}
+	}
+	return gearNumIdx
 }
 
 // grabParts takes all of the schematics and returns the
@@ -101,6 +180,12 @@ func parseLines(lines []string) []schematic {
 		symbols := symbRegex.FindAllStringIndex(lines[i], -1)
 		for j := 0; j < len(symbols); j++ {
 			line.symbols = append(line.symbols, symbols[j][0])
+		}
+
+		gearRegex := regexp.MustCompile((`\*+`))
+		gears := gearRegex.FindAllStringIndex(lines[i], -1)
+		for j := 0; j < len(gears); j++ {
+			line.gearSymbols = append(line.gearSymbols, gears[j][0])
 		}
 
 		schematics = append(schematics, line)
