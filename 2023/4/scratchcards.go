0a6457826e51bde6f5bd0046ec924de263cec40a
angelajfisher <31549337+angelajfisher@users.noreply.github.com>
2023-12-05 10:18:38 -0800

    Part 2 done!!

--- 2023/4/scratchcards.go
+++ 2023/4/scratchcards.go
@@ -14,6 +14,7 @@ type scratchcard struct {
 	id			int
 	winningNums	[]int
 	cardNums	[]int
+	copies		int
 }
 
 func main() {
@@ -27,8 +28,28 @@ func main() {
 	}
 
 	fmt.Printf("The answer to part one is: %v\n", points)
+
+	totalCards := 0
+	for i := 0; i < len(scratchcards); i++ {
+		wins := countWins(scratchcards[i])
+		scratchcards = addCards(i, int(wins), scratchcards)
+		totalCards += scratchcards[i].copies
+	}
+	fmt.Printf("The answer to part two is: %v\n", totalCards)
 }
 
+// addCards increases the number of copies of matching scratchcards and
+// returns the updated array of scratchcards
+func addCards(startIndex int, numToAdd int, scratchcards []scratchcard) []scratchcard {
+	for i := startIndex+1; i <= startIndex + numToAdd; i++ {
+		scratchcards[i].copies += scratchcards[startIndex].copies
+	}
+	return scratchcards
+}
+
+// countWins checks all the nums in a given scratchcard
+// against that card's winning numbers, then returns
+// the number of matches
 func countWins(scratchcard scratchcard) float64 {
 	var wins float64 = 0
 	for _, num := range scratchcard.winningNums {
@@ -48,6 +69,7 @@ func parseData(lines []string) []scratchcard {
 	scratchcards := []scratchcard{}
 	for i := 0; i < len(lines); i++ {
 		var scratchcard scratchcard
+		scratchcard.copies = 1
 
 		regex := regexp.MustCompile(`\d+`)
 		idIndex := regex.FindStringIndex(lines[i])
@@ -57,11 +79,11 @@ func parseData(lines []string) []scratchcard {
 		line := lines[i][idIndex[1]+2:]
 
 		allNums := strings.Split(line, "|")
-		winngingNums := regex.FindAllString(allNums[0], -1)
+		winningNums := regex.FindAllString(allNums[0], -1)
 		cardNums := regex.FindAllString(allNums[1], -1)
 
-		for i := 0; i < len(winngingNums); i++ {
-			num, _ := strconv.Atoi(winngingNums[i])
+		for i := 0; i < len(winningNums); i++ {
+			num, _ := strconv.Atoi(winningNums[i])
 			scratchcard.winningNums = append(scratchcard.winningNums, num)
 		}
 		for i := 0; i < len(cardNums); i++ {
